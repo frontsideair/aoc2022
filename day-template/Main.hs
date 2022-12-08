c64b544ff2c82a10dcc912c4ed479d6a989543be
Fatih Altinok <photonia@gmail.com>
2022-12-08 19:52:46 +0300

    Improve template

--- day-template/Main.hs
+++ day-template/Main.hs
@@ -1,16 +1,19 @@
+import Control.Monad (when)
+import System.Environment (getArgs)
 import Text.Parsec.String (Parser, parseFromFile)
 
 part1 :: IO ()
 part1 = do
   input <- parseFromFile parser "input.txt" >>= either (error . show) return
-  undefined
+  return ()
 
 part2 :: IO ()
 part2 = do
   input <- parseFromFile parser "input.txt" >>= either (error . show) return
-  undefined
+  return ()
 
 main :: IO ()
 main = do
-  part1
-  part2
+  arg <- head . (++ ["all"]) <$> getArgs
+  when (arg == "all" || arg == "part1") part1
+  when (arg == "all" || arg == "part2") part2
