99cc9ba8d9619ad1a7ff1a9ecf0470b5a55071d8
Fatih Altinok <photonia@gmail.com>
2022-12-11 09:59:04 +0300

    Day 11 part 1

---
+++ day-11/Main.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+import Control.Applicative ((<|>))
+import Control.Monad (when)
+import Data.Function ((&))
+import Data.Functor (($>))
+import Data.List (foldl', sortOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Map.Strict ((!))
+import Data.Ord (Down (Down))
+import System.Environment (getArgs)
+import Text.Parsec (digit, many1, sepBy, sepEndBy, spaces, string)
+import Text.Parsec.Char (newline)
+import Text.Parsec.Combinator (eof)
+import Text.Parsec.String (Parser, parseFromFile)
+import Prelude hiding (round)
+
+part1 :: IO ()
+part1 = do
+  input <- parseFromFile parser "input.txt" >>= either (error . show) return
+  print $ heldItems <$> input
+  -- print (($ 13) . operation <$> input)
+  print $ heldItems <$> turn input 0
+  print $ heldItems <$> round input
+  print $ monkeyBusiness $ iterate round input !! 20
+  return ()
+
+turn :: Map Int Monkey -> Int -> Map Int Monkey
+turn monkeys index = case monkeys ! index of
+  Monkey {heldItems = []} -> monkeys
+  monkey@(Monkey operation (Test condition ifTrue ifFalse) (item : rest) inspected) -> turn monkeys' index
+    where
+      worryLevel = operation item `div` 3
+      conditionResult = worryLevel `mod` condition == 0
+      monkeyToThrow = if conditionResult then ifTrue else ifFalse
+      monkey' = monkey {heldItems = rest, inspected = inspected + 1}
+      monkeys' =
+        monkeys
+          & Map.adjust (\monkey@Monkey {heldItems} -> monkey {heldItems = heldItems ++ [worryLevel]}) monkeyToThrow
+          & Map.insert index monkey'
+
+round :: Map Int Monkey -> Map Int Monkey
+round monkeys = foldl' turn monkeys (Map.keys monkeys)
+
+monkeyBusiness :: Map Int Monkey -> Int
+monkeyBusiness monkeys = product $ take 2 $ sortOn Down $ inspected <$> Map.elems monkeys
+
+part2 :: IO ()
+part2 = do
+  input <- parseFromFile parser "input.txt" >>= either (error . show) return
+  return ()
+
+data Test = Test {condition :: Int, ifTrue :: Int, ifFalse :: Int} deriving (Show)
+
+data Monkey = Monkey {operation :: Int -> Int, test :: Test, heldItems :: [Int], inspected :: Int}
+
+parser :: Parser (Map Int Monkey)
+parser = Map.fromList <$> monkey `sepEndBy` newline <* eof
+
+monkey :: Parser (Int, Monkey)
+monkey = do
+  string "Monkey "
+  index <- decimal
+  string ":"
+  spaces
+  string "Starting items: "
+  heldItems <- decimal `sepBy` string ", "
+  spaces
+  string "Operation: new = old "
+  operation <- op
+  spaces
+  string "Test: divisible by "
+  condition <- decimal
+  spaces
+  string "If true: throw to monkey "
+  ifTrue <- decimal
+  spaces
+  string "If false: throw to monkey "
+  ifFalse <- decimal
+  newline
+  return (index, Monkey operation (Test condition ifTrue ifFalse) heldItems 0)
+
+decimal :: Parser Int
+decimal = read <$> many1 digit
+
+op :: Parser (Int -> Int)
+op = do
+  f <- (string "* " $> (*)) <|> (string "+ " $> (+))
+  f <$> decimal <|> (string "old" $> (\a -> a `f` a))
+
+main :: IO ()
+main = do
+  arg <- head . (++ ["all"]) <$> getArgs
+  when (arg == "all" || arg == "part1") part1
+  when (arg == "all" || arg == "part2") part2
