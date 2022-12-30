36e2b4bb0f0884ddb6e3e69412681bc5ded8704d
Fatih Altinok <photonia@gmail.com>
2022-12-30 15:23:25 +0300

    Day 22 part 2

--- day-22/Main.hs
+++ day-22/Main.hs
@@ -23,22 +23,83 @@ part1 = do
   (map, path) <- parseFromFile parser "input.txt" >>= either (error . show) return
   let state = (startingPoint map, startingDirection)
   -- print (map, path, state)
-  print $ score $ foldl' (step map) state path
+  print $ score $ foldl' (step jump map) state path
   return ()
 
-step :: Map Coord Cell -> (Coord, Direction) -> Instruction -> (Coord, Direction)
-step map (coord, direction) TurnLeft = (coord, turnLeft direction)
-step map (coord, direction) TurnRight = (coord, turnRight direction)
-step map (coord, direction) (Move steps) = (go steps coord, direction)
+step :: (Map Coord Cell -> (Coord, Direction) -> (Coord, Direction)) -> Map Coord Cell -> (Coord, Direction) -> Instruction -> (Coord, Direction)
+step _ _ (coord, direction) TurnLeft = (coord, turnLeft direction)
+step _ _ (coord, direction) TurnRight = (coord, turnRight direction)
+step jump map (coord, direction) (Move steps) = go steps (coord, direction)
   where
-    go n coord = if n == 0 || map ! coord' == Wall then coord else go (n - 1) coord' where coord' = proceed map direction coord
+    go n (coord, direction) = if n == 0 || map ! coord' == Wall then (coord, direction) else go (n - 1) (coord', direction')
+      where
+        (coord', direction') = if Map.member candidate map then (candidate, direction) else jump map (coord, direction)
+        candidate = move coord direction
 
-proceed :: Map Coord Cell -> Direction -> Coord -> Coord
-proceed map direction (y, x) = case direction of
-  North -> if Map.member candidate map then candidate else last $ filter ((== x) . snd) $ Map.keys map where candidate = (y - 1, x)
-  East -> if Map.member candidate map then candidate else head $ filter ((== y) . fst) $ Map.keys map where candidate = (y, x + 1)
-  South -> if Map.member candidate map then candidate else head $ filter ((== x) . snd) $ Map.keys map where candidate = (y + 1, x)
-  West -> if Map.member candidate map then candidate else last $ filter ((== y) . fst) $ Map.keys map where candidate = (y, x - 1)
+move :: Coord -> Direction -> Coord
+move (y, x) North = (y - 1, x)
+move (y, x) East = (y, x + 1)
+move (y, x) South = (y + 1, x)
+move (y, x) West = (y, x - 1)
+
+jump :: Map Coord Cell -> (Coord, Direction) -> (Coord, Direction)
+jump map ((y, x), direction) = (coord', direction)
+  where
+    keys = Map.keys map
+    coord' = case direction of
+      North -> last $ filter ((== x) . snd) keys
+      East -> head $ filter ((== y) . fst) keys
+      South -> head $ filter ((== x) . snd) keys
+      West -> last $ filter ((== y) . fst) keys
+
+{-
+  TTRR
+  TTRR
+  FF
+  FF
+LLBB
+LLBB
+HH
+HH
+-}
+
+jump' :: Map Coord Cell -> (Coord, Direction) -> (Coord, Direction)
+jump' map ((y, x), direction) = (coord', direction')
+  where
+    currentFace = findCurrentFace (y, x)
+    (_, direction', coord') = case (currentFace, direction) of
+      (TopFace, North) -> (HindFace, East, (x + 100, y)) -- (1, 51) -> (151, 1), (1, 100) -> (200, 1)
+      (TopFace, West) -> (LeftFace, East, (151 - y, x - 50)) -- (1, 51) -> (150, 1), (50, 51) -> (101, 1)
+      (RightFace, North) -> (HindFace, North, (y + 199, x - 100)) -- (1, 101) -> (200, 1), (1, 150) -> (200, 50)
+      (RightFace, East) -> (BottomFace, West, (151 - y, x - 50)) -- (1, 150) -> (150, 100), (50, 150) -> (101, 100)
+      (RightFace, South) -> (FrontFace, West, (x - 50, y + 50)) -- (50, 101) -> (51, 100), (50, 150) -> (100, 100)
+      (FrontFace, West) -> (LeftFace, South, (x + 50, y - 50)) -- (51, 51) -> (101, 1), (100, 51) -> (101, 50)
+      (FrontFace, East) -> (RightFace, North, (x - 50, y + 50)) -- (51, 100) -> (50, 101), (100, 100) -> (50, 150)
+      (LeftFace, West) -> (TopFace, East, (151 - y, x + 50)) -- (101, 1) -> (50, 51), (150, 1) -> (1, 51)
+      (LeftFace, North) -> (FrontFace, East, (x + 50, y - 50)) -- (101, 1) -> (51, 51), (101, 50) -> (100, 51)
+      (BottomFace, South) -> (HindFace, West, (x + 100, y - 100)) -- (150, 51) -> (151, 50), (150, 100) -> (200, 50)
+      (BottomFace, East) -> (RightFace, West, (151 - y, x + 50)) -- (101, 100) -> (50, 150), (150, 100) -> (1, 150)
+      (HindFace, West) -> (TopFace, South, (x, y - 100)) -- (151, 1) -> (1, 51), (200, 1) -> (1, 100)
+      (HindFace, South) -> (RightFace, South, (y - 199, x + 100)) -- (200, 1) -> (1, 101), (200, 50) -> (1, 150)
+      (HindFace, East) -> (BottomFace, North, (x + 100, y - 100)) -- (151, 50) -> (150, 51), (200, 50) -> (150, 100)
+      _ -> error "not found"
+
+data Face = TopFace | FrontFace | RightFace | HindFace | LeftFace | BottomFace deriving (Show, Eq)
+
+findCurrentFace :: Coord -> Face
+findCurrentFace coord
+  | coord `within` (1, 51) = TopFace
+  | coord `within` (1, 101) = RightFace
+  | coord `within` (51, 51) = FrontFace
+  | coord `within` (101, 1) = LeftFace
+  | coord `within` (101, 51) = BottomFace
+  | coord `within` (151, 1) = HindFace
+  | otherwise = error "not found"
+
+within :: Coord -> Coord -> Bool
+within (y, x) (yMin, xMin) = y `w` (yMin, yMin + 50) && x `w` (xMin, xMin + 50)
+  where
+    w x (min, max) = x >= min && x < max
 
 score :: (Coord, Direction) -> Int
 score (coord, dir) = scoreCoord coord + scoreDir dir
@@ -51,7 +112,9 @@ score (coord, dir) = scoreCoord coord + scoreDir dir
 
 part2 :: IO ()
 part2 = do
-  input <- parseFromFile parser "input.txt" >>= either (error . show) return
+  (map, path) <- parseFromFile parser "input.txt" >>= either (error . show) return
+  let state = (startingPoint map, startingDirection)
+  print $ score $ foldl' (step jump' map) state path
   return ()
 
 parser :: Parser (Map Coord Cell, [Instruction])
